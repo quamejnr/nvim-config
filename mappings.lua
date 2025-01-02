5b9d1647bbe330305319b4b7461565aa0cf5574c
Kwame Akuamoah-Boateng <kaboateng14@gmail.com>
2025-01-02 08:21:51 +0000

    Fix trouble diagnostics and floating terminal

--- mappings.lua
+++ mappings.lua
@@ -13,12 +13,23 @@ M.general = {
   n = {
     [";"] = { ":", "enter command mode", opts = { nowait = true } },
     -- ["<C-f>"] = { "<cmd>silent !tmux new tmux-sessionizer<CR>" },
-    ["<leader>q"] = { "<cmd>Trouble workspace_diagnostics<CR>" },
+    ["<leader>q"] = { "<cmd>Trouble diagnostics<CR>" },
   },
 
   v = {
     [">"] = { ">gv", "indent" },
   },
+}
+
+M.nvterm = {
+  n = {
+    ["<leader>i"] = {
+      function()
+        require("nvterm.terminal").toggle "float"
+      end,
+      "Toggle floating term",
+    },
+  }
 }
 
 return M
